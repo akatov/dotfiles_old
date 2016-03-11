20d4328e80c77c6f645d09143098bad74a5bb54d
Dmitri Akatov <akatov@gmail.com>
2016-03-11 00:54:32 +0000

    emacs: moved ace-jump-mode config to config.org

--- emacs.d/shared/hooks/init-ace-jump-mode.el
+++
@@ -1,9 +0,0 @@
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-(global-set-key (kbd "C-c C-SPC") 'ace-jump-mode)
-(global-set-key (kbd "C-;") 'ace-jump-mode)
