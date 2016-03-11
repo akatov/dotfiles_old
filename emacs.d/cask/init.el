35418d59c7c5d6275e9dcba0a08d5963d0ea8fac
Dmitri Akatov <akatov@gmail.com>
2016-03-11 00:03:30 +0000

    moving config from init.el to config.org

--- emacs.d/cask/init.el
+++ emacs.d/cask/init.el
@@ -2,34 +2,13 @@
 (org-babel-load-file
  (concat user-emacs-directory "config.org"))
 
-(setq exec-path (append exec-path '("/usr/local/bin")))
-
-;; always ask y-or-n questions
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; Movement between windows
-(global-set-key "\M-g\M-h" 'windmove-left)
-(global-set-key "\M-g\M-l" 'windmove-right)
-(global-set-key "\M-g\M-k" 'windmove-up)
-(global-set-key "\M-g\M-j" 'windmove-down)
-(global-set-key "\M-gh" 'windmove-left)
-(global-set-key "\M-gl" 'windmove-right)
-(global-set-key "\M-gk" 'windmove-up)
-(global-set-key "\M-gj" 'windmove-down)
-
 ;; fullscreen
-(defun toggle-fullscreen ()
-  "Toggle full screen"
-  (interactive)
-  (set-frame-parameter
-   nil 'fullscreen (unless (frame-parameter nil 'fullscreen) 'fullboth)))
-(global-set-key (kbd "s-<return>") 'toggle-fullscreen)
-
-(ido-mode 'buffers)
-
-;; load custom
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file 'noerror)
+;; (defun toggle-fullscreen ()
+;;   "Toggle full screen"
+;;   (interactive)
+;;   (set-frame-parameter
+;;    nil 'fullscreen (unless (frame-parameter nil 'fullscreen) 'fullboth)))
+;; (global-set-key (kbd "s-<return>") 'toggle-fullscreen)
 
 ;; load hooks
 (defun add-hooks (mode-hooks hooks)
@@ -41,6 +20,3 @@
 (mapc (lambda (f) (load f)) (f-files "~/.emacs.d/private"))
 
 (setq inferior-js-program-command "node")
-
-(when (memq window-system '(ns))
-  (exec-path-from-shell-initialize))
